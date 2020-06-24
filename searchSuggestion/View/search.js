15c55d2c90478c2f1ee7331a34622ee056fdad93
lixuan <lilx@unc.edu>
2020-06-24 00:44:17 -0400

    Display search results in html

--- searchSuggestion/View/search.js
+++ searchSuggestion/View/search.js
@@ -1,10 +1,11 @@
 
 $( document ).ready(function() {
   console.log( "ready!" );
+  list   = document.querySelector("#list")
 
-// Load and return local json data
-// I wasnt sure jquery allows loading json data from local. 
-// Its probably also not the greatest idea because of security reasons 
+  // Load and return json data
+  // I wasnt sure jquery allows loading json data from local. 
+  // Its probably also not the greatest idea because of security reasons 
   $.getJSON("https://raw.githubusercontent.com/LeekSwan/developerTests/master/searchSuggestion/search.json", function(response){
     const fuse = new Fuse(response, {
       keys: ['word'],
@@ -14,12 +15,22 @@ $( document ).ready(function() {
       includeMatches: true
     });
 
-      // Reads input box text
+    // Reads input box text
     $('#target').on('keyup', function () {
       let result = fuse.search($(this).val());
       // Output it
       console.log(result)
       
+      // Clears div so that it doesnt keep appending items.
+      $("#list").empty();
+
+      // Iterates through results and appends it to list
+      let resultdiv = $('#list');
+        for (i = 0; i < Object.keys(result).length; i++) {
+          let searchitem = '<li>' + result[i].item.word + '</li>'
+          resultdiv.append(searchitem);
+        }
+
     });
   });
 });
@@ -27,11 +38,4 @@ $( document ).ready(function() {
 
 
 
-
-
-
-  // inject html back into list
-const inject = function() {
-  document.getElementById('printchatbox').innerHTML = readInput();
-}
 
