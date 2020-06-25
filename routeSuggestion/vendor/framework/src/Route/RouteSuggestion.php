13643b79b0744dccd4819729475855d8eb3a46da
lixuan <lilx@unc.edu>
2020-06-25 14:32:14 -0400

    final

--- routeSuggestion/vendor/framework/src/Route/RouteSuggestion.php
+++ routeSuggestion/vendor/framework/src/Route/RouteSuggestion.php
@@ -4,5 +4,12 @@ namespace Route;
 
 class RouteSuggestion
 {
-    //
+    // When routing fails, catch error and point to RouteSuggestion
+
+    // Run function to check closest match in route library. Similar to how searchSuggestion works
+        // Index through route library
+        // Use characters from input to check against route library
+        // Return closest match
+
+    // return correct route 
 }
--- searchSuggestion/View/search.js
+++ searchSuggestion/View/search.js
@@ -1,38 +1,53 @@
 
 $( document ).ready(function() {
   console.log( "ready!" );
-  list   = document.querySelector("#list")
+  list = document.querySelector("#list")
 
   // Load and return json data
   // I wasnt sure jquery allows loading json data from local. 
-  // Its probably also not the greatest idea because of security reasons 
+  // Its probably also not the greatest idea because of security reasons
+  // Also runs fuse which is a lightweight fuzzy search library for JS.  
   $.getJSON("https://raw.githubusercontent.com/LeekSwan/developerTests/master/searchSuggestion/search.json", function(response){
     const fuse = new Fuse(response, {
       keys: ['word'],
       distance: 100,
       minMatchCharLength: 2,
-      threshold: 0.5,
-      includeMatches: true
+      threshold: 0.5, 
+      includeMatches: true // This returns the index of matched characters but since we are working with objects, this may be diffucult to use for highlighting. 
     });
 
     // Reads input box text
     $('#target').on('keyup', function () {
-      let result = fuse.search($(this).val());
-      // Output it
-      console.log(result)
-      
+      char_input = $(this).val();
+      let result = fuse.search(char_input); // Calls fuse on input characters
+
       // Clears div so that it doesnt keep appending items.
       $("#list").empty();
 
       // Iterates through results and appends it to list
       let resultdiv = $('#list');
         for (i = 0; i < Object.keys(result).length; i++) {
+          word = result[i].item.word;
+
+          // // This would be what bolds the searched characters but i couldnt get it to quite work. 
+          // console.log(word.replace(/[char_input]/g, m => char_object(char_input)[m]));
+
           let searchitem = '<li>' + result[i].item.word + '</li>'
           resultdiv.append(searchitem);
         }
-
     });
   });
+
+  // // Creates object for replace to iterate through
+  // function char_object(char_input) {
+  //   var char_array = {}
+
+  //   for (i = 0; i < char_input.length; i++) {
+  //     char_array[char_input[i].toString()] = '<b>' + char_input[i] + '</b>';
+  //   }
+  //   return char_array
+  // }
+
 });
 
 
